e40bcd122236673c3cb0d4819c5245f8d0b2b7e0
jjacquel <>
2021-08-10 20:57:16 +0300

    colored promt

--- minishell.h
+++ minishell.h
@@ -15,6 +15,9 @@
 # include <sys/errno.h>
 # include <sys/stat.h>
 # include <signal.h>
+#define GREEN "\001\033[32m\002"
+#define RED "\001\033[31m\002"
+#define RESET   "\001\033[0m\002"
 
 int 	g_status;
 
